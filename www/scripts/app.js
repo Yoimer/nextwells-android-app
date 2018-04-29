66906068012802f5716d75f5165e1665d253ce80
Yoimer <ydrf19@gmail.com>
2018-04-29 02:43:22 -0400

    testing working code from simulation

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -38,116 +38,142 @@ var app = {
         document.addEventListener("deviceready", onDeviceReady, false);
 
         var currentRow;
-        // Populate the database
-         //
+        // Populate the phone_gap
+        //
         function populateDB(tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name,number)');
-            }
+          tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name,number)');
+        }
 
-        // Query the database
+        // Query the phone_gap
         //
         function queryDB(tx) {
-            tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
-            }
+          tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+        }
 
         function searchQueryDB(tx) {
-            tx.executeSql("SELECT * FROM DEMO where name like ('%"+ document.getElementById("txtName").value + "%')", [], querySuccess, errorCB);
-            }
-            // Query the success callback
-            //
+          tx.executeSql("SELECT * FROM DEMO where name like ('%"+ document.getElementById("txtName").value + "%')",
+          [], querySuccess, errorCB);
+        }
+        // Query the success callback
+        //
         function querySuccess(tx, results) {
-            var tblText = '<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
-            var len = results.rows.length;
-            for (var i = 0; i < len; i++) {
-                var tmpArgs = results.rows.item(i).id + ",'" + results.rows.item(i).name + "','" + results.rows.item(i).number+"'";
-                tblText += '<tr onclick="goPopup('+ tmpArgs + ');"><td>' + results.rows.item(i).id +'</td><td>' + results.rows.item(i).name +'</td><td>' + results.rows.item(i).number +'</td></tr>';
-                //tblText += '<tr class="go-popup"><td>' + results.rows.item(i).id +'</td><td>' + results.rows.item(i).name +'</td><td>' + results.rows.item(i).number +'</td></tr>';
-                }
-                tblText += "</table>";
-                document.getElementById("tblDiv").innerHTML = tblText;
-            }
+          var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
+          var len = results.rows.length;
+          for (var i = 0; i < len; i++) {
+        var tmpArgs=results.rows.item(i).id + ",'" + results.rows.item(i).name
+        + "','" + results.rows.item(i).number+"'";
+        tblText +='<tr onclick="goPopup('+ tmpArgs + ');"><td>' + results.rows.item(i).id +'</td><td>'
+        + results.rows.item(i).name +'</td><td>' + results.rows.item(i).number +'</td></tr>';
+          }
+          tblText +="</table>";
+          document.getElementById("tblDiv").innerHTML =tblText;
+        }
 
         //Delete query
         function deleteRow(tx) {
-            tx.executeSql('DELETE FROM DEMO WHERE id = ' + currentRow, [], queryDB, errorCB);
-            }
+          tx.executeSql('DELETE FROM DEMO WHERE id = ' + currentRow, [], queryDB, errorCB);
+        }
 
         // Transaction error callback
         //
         function errorCB(err) {
-            alert("Error processing SQL: "+err.code);
-            }
+          alert("Error processing SQL: "+err.code);
+        }
 
         // Transaction success callback
         //
         function successCB() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(queryDB, errorCB);
-            }
+          var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+          db.transaction(queryDB, errorCB);
+        }
 
-        // Cordova is ready
-        //
-        function onDeviceReady() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(populateDB, errorCB, successCB);
-            }
+        //  // Cordova is ready
+        // //
+        // function onDeviceReady() {
+        //   var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+        //   db.transaction(populateDB, errorCB, successCB);
+        // }
 
         //Insert query
         //
         function insertDB(tx) {
-            tx.executeSql('INSERT INTO DEMO (name,number) VALUES ("' +document.getElementById("txtName").value +'","'+document.getElementById("txtNumber").value+'")');
+          tx.executeSql('INSERT INTO DEMO (name,number) VALUES ("' +document.getElementById("txtName").value
+          +'","'+document.getElementById("txtNumber").value+'")');
         }
 
-        //goInsert()
+        function goInsert() {
+          //onDeviceReady();
+          var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+          db.transaction(insertDB, errorCB, successCB);
+        }
+
+        function goSearch() {
+          var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+          db.transaction(searchQueryDB, errorCB);
+        }
+
+        function goDelete() {
+           var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+           db.transaction(deleteRow, errorCB);
+           document.getElementById('qrpopup').style.display='none';
+        }
+
+        //Show the popup after tapping a row in table
+        //
+        function goPopup(row,rowname,rownum) {
+          currentRow=row;
+          document.getElementById("qrpopup").style.display="block";
+          document.getElementById("editNameBox").value = rowname;
+          document.getElementById("editNumberBox").value = rownum;
+        }
+
+        function editRow(tx) {
+          tx.executeSql('UPDATE DEMO SET name ="'+document.getElementById("editNameBox").value+
+          '", number= "'+document.getElementById("editNumberBox").value+ '" WHERE id = '
+          + currentRow, [], queryDB, errorCB);
+        }
+        function goEdit() {
+          var db = window.openDatabase("phone_gap", "0.10", "Cordova Demo", 200000);
+          db.transaction(editRow, errorCB);
+          document.getElementById('qrpopup').style.display='none';
+        }
+
+        function goDiscard() {
+          document.getElementById('qrpopup').style.display='none';
+        }
+
+        //inputs handler
+
+        //goInsert();
         document.querySelector('.go-insert').addEventListener('click', function() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(insertDB, errorCB, successCB);
+          goInsert();
         });
 
         //goSearch()
         document.querySelector('.go-search').addEventListener('click', function() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(searchQueryDB, errorCB);
+          goSearch();
         });
 
-        //goDelete()
-        document.querySelector('.go-delete').addEventListener('click', function() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(deleteRow, errorCB);
-            document.getElementById('qrpopup').style.display='none';
-        });
-
-        // //Show the popup after tapping a row in table
-        function goPopup(row,rowname,rownum) {
-            currentRow = row;
-            document.getElementById("qrpopup").style.display = "block";
-            document.getElementById("editNameBox").value = rowname;
-            document.getElementById("editNumberBox").value = rownum;
-        }
-
-        function editRow(tx) {
-            tx.executeSql('UPDATE DEMO SET name ="' + document.getElementById("editNameBox").value + '", number= "'+document.getElementById("editNumberBox").value + '" WHERE id = ' + currentRow, [], queryDB, errorCB);
-        }
-
-        //goEdit()
-        document.querySelector('.go-edit').addEventListener('click', function() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(editRow, errorCB);
-            document.getElementById('qrpopup').style.display = 'none';
-        });
-
-        //#discard
-        document.querySelector('#discard').addEventListener('click', function() {
-            document.getElementById('qrpopup').style.display='none';
-        });
-
-        //successCB()
+        //go-successCB
         document.querySelector('.go-successCB').addEventListener('click', function() {
-            var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-            db.transaction(queryDB, errorCB);
+          successCB();
         });
 
-    }
+        //go-delete
+        document.querySelector('.go-delete').addEventListener('click', function() {
+          goDelete();
+        });
+
+        //go-edit
+        document.querySelector('.go-edit').addEventListener('click', function() {
+          goEdit();
+        });
+
+        //discard
+        document.querySelector('#discard').addEventListener('click', function() {
+          goDiscard();
+        });
+
 };
 
 app.initialize();
