65adc1a9555e3a882a4615dbade847b210678bb4
Yoimer <ydrf19@gmail.com>
2018-05-07 14:56:02 -0400

    added ajax-example folder

---
+++ www/ajax-example/test.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo '#12345-Yoimer#6789-Roman';
+    // edit .htaccess
+    /* # HTID:3569822: DO NOT REMOVE OR MODIFY THIS LINE AND THE LINES BELOW
+    php_value display_errors 1
+    // this what I added - start
+    Header add Access-Control-Allow-Origin "*"
+    // this what I added - end
+    # DO NOT REMOVE OR MODIFY THIS LINE AND THE LINES ABOVE HTID:3569822:
+    */
+?>
